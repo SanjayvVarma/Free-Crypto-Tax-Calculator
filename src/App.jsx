5c5893936344567a8ec5c27461e0a6d0e678b92c
SanjayvVarma <spverma.sanjay56056@gmail.com>
2024-10-21 08:47:48 +0530

    navbar complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Calculator from './components/Calculator'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Calculator/>
+    </>
+  )
+}
+
+export default App
